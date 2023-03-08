9695456c567130875683fe38b7cdd19a867874c6
Cigdem Akbas <cigdemtazegul@hotmail.com>
2023-03-08 17:03:12 -0500

    Optimizing

--- src/test/java/com/ecommerce/pages/FootLinksPages.java
+++ src/test/java/com/ecommerce/pages/FootLinksPages.java
@@ -12,98 +12,98 @@ public class FootLinksPages extends Utility {
 
     public void checkCategoriesLink() {
         fle = new FootLinksElements();
-        Utility.scrollTo(fle.womenLink);
-        Utility.waits(2);
+        scrollTo(fle.womenLink);
+        waits(2);
         fle.womenLink.click();
-        Utility.waits(2);
+        waits(2);
         Assert.assertTrue(fle.womenLinkPageHeader.isDisplayed());
     }
 
     public void checkInformationLinks() {
         fle = new FootLinksElements();
-        Utility.scrollToCenter(fle.specialLink);
-        Utility.waits(2);
+        scrollToCenter(fle.specialLink);
+        waits(2);
         fle.specialLink.click();
-        Utility.waits(2);
+        waits(2);
         Assert.assertTrue(fle.specialLinkPageHeader.isDisplayed());
 
-        Utility.scrollTo(fle.newProductsLink);
-        Utility.waits(1);
+        scrollTo(fle.newProductsLink);
+        waits(1);
         fle.newProductsLink.click();
-        Utility.waits(2);
+        waits(2);
         String actualUrl = "https://ecommerce.yosemiteint.com/prestashop/index.php?controller=new-products";
         String expectedUrl = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl, actualUrl, "NOT MATCHED");
 
-        Utility.scrollTo(fle.bestSellersLink);
-        Utility.waits(1);
+        scrollTo(fle.bestSellersLink);
+        waits(1);
         fle.bestSellersLink.click();
-        Utility.waits(2);
+        waits(2);
         String actualUrl1 = "https://ecommerce.yosemiteint.com/prestashop/index.php?controller=best-sales";
         String expectedUrl1 = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl1, actualUrl1, "NOT MATCHED");
 
-        Utility.scrollTo(fle.ourStores);
+        scrollTo(fle.ourStores);
         fle.ourStores.click();
         String actualUrl2 = "https://ecommerce.yosemiteint.com/prestashop/index.php?controller=stores";
         String expectedUrl2 = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl2, actualUrl2, "NOT MATCHED");
 
-        Utility.scrollTo(fle.contactUs);
+        scrollTo(fle.contactUs);
         fle.contactUs.click();
         String actualUrl3 = "https://ecommerce.yosemiteint.com/prestashop/index.php?controller=contact";
         String expectedUrl3 = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl3, actualUrl3, "NOT MATCHED");
 
-        Utility.scrollTo(fle.termsAndConditions);
+        scrollTo(fle.termsAndConditions);
         fle.termsAndConditions.click();
         String actualUrl4 = "https://ecommerce.yosemiteint.com/prestashop/index.php?id_cms=3&controller=cms";
         String expectedUrl4 = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl4, actualUrl4, "NOT MATCHED");
 
-        Utility.scrollTo(fle.aboutUs);
+        scrollTo(fle.aboutUs);
         fle.aboutUs.click();
         String actualUrl5 = "https://ecommerce.yosemiteint.com/prestashop/index.php?id_cms=4&controller=cms";
         String expectedUrl5 = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl5, actualUrl5, "NOT MATCHED");
-        Utility.waits(2);
+        waits(2);
 
-        Utility.scrollTo(fle.sitemap);
+        scrollTo(fle.sitemap);
         fle.sitemap.click();
         String actualUrl6 = "https://ecommerce.yosemiteint.com/prestashop/index.php?controller=sitemap";
         String expectedUrl6 = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl6, actualUrl6, "NOT MATCHED");
-        Utility.waits(2);
+        waits(2);
     }
     public void myAccountLinks() {
         fle = new FootLinksElements();
-        Utility.scrollTo(fle.myOrders);
+        scrollTo(fle.myOrders);
         fle.myOrders.click();
         String actualUrl = "https://ecommerce.yosemiteint.com/prestashop/index.php?controller=authentication&back=history";
         String expectedUrl = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl, actualUrl, "NOT MATCHED");
-        Utility.waits(1);
+        waits(1);
 
-        Utility.scrollTo(fle.myCreditSlips);
+        scrollTo(fle.myCreditSlips);
         fle.myCreditSlips.click();
         String actualUrl1 = "https://ecommerce.yosemiteint.com/prestashop/index.php?controller=authentication&back=order-slip";
         String expectedUrl1 = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl1, actualUrl1, "NOT MATCHED");
-        Utility.waits(1);
+        waits(1);
 
-        Utility.scrollTo(fle.myAddresses);
+        scrollTo(fle.myAddresses);
         fle.myAddresses.click();
         String actualUrl2 = "https://ecommerce.yosemiteint.com/prestashop/index.php?controller=authentication&back=addresses";
         String expectedUrl2 = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl2, actualUrl2, "NOT MATCHED");
-        Utility.waits(1);
+        waits(1);
 
         Utility.scrollTo(fle.myPersonalInfo);
         fle.myPersonalInfo.click();
         String actualUrl3 = "https://ecommerce.yosemiteint.com/prestashop/index.php?controller=authentication&back=identity";
         String expectedUrl3 = Driver.getDriver().getCurrentUrl();
         Assert.assertEquals(expectedUrl3, actualUrl3, "NOT MATCHED");
-        Utility.waits(1);
+        waits(1);
     }
 }
 
--- src/test/java/com/ecommerce/pages/TestTheNavigationPages.java
+++ src/test/java/com/ecommerce/pages/TestTheNavigationPages.java
@@ -10,34 +10,31 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
-public class TestTheNavigationPages {
+public class TestTheNavigationPages extends Utility {
 
     TestTheNavigationElements tne;
-
-    public String email = ConfigReader.getProperty("email");
-    public String password = "";
 
     public void checkingWomenBar() {
         tne = new TestTheNavigationElements();
         tne.womenTab.click();
         Assert.assertTrue(tne.womenPageHeader.isDisplayed());
-        Utility.waits(1);
-        Utility.hoverOver( tne.womenTab2, 2);
-        Utility.waits(1);
+        waits(1);
+        hoverOver( tne.womenTab2, 2);
+        waits(1);
         tne.tShirtLink.click();
-        Utility.waits(2);
+        waits(2);
         Assert.assertTrue(tne.tShirtPageHeader.isDisplayed());
-        Utility.hoverOver( tne.womenTab3, 2);
+        hoverOver( tne.womenTab3, 2);
         tne.blouseLink.click();
-        Utility.waits(1);
+        waits(1);
         Assert.assertTrue(tne.blouseLinkPageHeader.isDisplayed());
-        Utility.hoverOver(tne.womenTab4, 1);
+        hoverOver(tne.womenTab4, 1);
         tne.casualDress.click();
         Assert.assertTrue(tne.casualDressPageHeader.isDisplayed());
-        Utility.hoverOver( tne.womenTab5, 1);
+        hoverOver( tne.womenTab5, 1);
         tne.eveningDress.click();
         Assert.assertTrue(tne.eveningDressPageHeader.isDisplayed());
-        Utility.hoverOver( tne.womenTab6, 1);
+        hoverOver( tne.womenTab6, 1);
         tne.summerDress.click();
         Assert.assertTrue(tne.summerDressPageHeader.isDisplayed());
     }
@@ -45,30 +42,29 @@ public class TestTheNavigationPages {
     public void checkingDressBar() {
         tne = new TestTheNavigationElements();
         tne.dressesBar.click();
-        Utility.waits(1);
+        waits(1);
         Assert.assertTrue(tne.dressesBarPageHeader.isDisplayed());
-        Utility.waits(1);
-        Utility.hoverOver(tne.dressesBar1, 2);
-        WebElement casualDressLink = Driver.getDriver().findElement(By.xpath("(//a[@title='Casual Dresses'][normalize-space()='Casual Dresses'])[2]"));
-        casualDressLink.click();
-        Utility.waits(2);
+        waits(1);
+        hoverOver(tne.dressesBar1, 2);
+        tne.casualDressLink.click();
+        waits(2);
         Assert.assertTrue(tne.casualDressLinkPageHeader.isDisplayed());
-        Utility.waits(1);
-        Utility.hoverOver( tne.dressesBar2, 2);
+        waits(1);
+        hoverOver( tne.dressesBar2, 2);
         tne.eveningDressLink.click();
-        Utility.waits(2);
+        waits(2);
         Assert.assertTrue(tne.eveningDressLinkPageHeadr.isDisplayed());
-        Utility.waits(1);
+        waits(1);
         Utility.hoverOver(tne.dressesBar3, 2);
         tne.summerDressLink.click();
-        Utility.waits(2);
+        waits(2);
         Assert.assertTrue(tne.summerDressLinkPageHeader.isDisplayed());
     }
     public void checkingTShirtsBar() {
         tne = new TestTheNavigationElements();
         tne.tshirtsBar.click();
         Assert.assertTrue(tne.tshirtsBarPageHeader.isDisplayed());
-        Utility.waits(2);
+        waits(2);
     }
 
 }
